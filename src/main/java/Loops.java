7c370071a4826ade4e4b44472d040a0b07af9b5a
Luis Garcia Perez <slashlg@hotmail.com>
2025-11-25 00:26:10 -0600

    retobit finalizado Luis Garcia Perez

--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -8,10 +8,23 @@ public class Loops {
      * Resultado esperado: 24
      */
     public static int sumArrayWhile() {
+        // Definimos un array de enteros
         int[] numbers = {4, 6, 9, 5};
         // Tu código aquí (usa while)
+        // Variable para acumular la suma
+        int sum = 0;
+        // Índice para recorrer el array
+        int i = 0;
+        // Mientras i sea menor que la longitud del array
+        while (i < numbers.length) {
+            // Se acumula el valor en la suma
+            sum += numbers[i];
+            // Avanzamos al siguiente índice
+            i++;
+        }
         // consejo: declara la variable "sum" fuera del loop
-        return 0; // Sustituye el 0 por la variable sum
+        // Sustituye el 0 por la variable sum
+        return sum;
     }
 
     /**
@@ -23,10 +36,26 @@ public class Loops {
      * Resultado esperado: 3
      */
     public static int countEvenNumbersDoWhile() {
+        // Definimos un array de enteros
         int[] numbers = {4, 6, 9, 5, 8};
         // Tu código aquí (usa do while)
+        // Variable para contar los pares
+     int count = 0;
+        // Índice para recorrer el array
+     int i =0;
 
-        return 0; // Sustituye el 0 por la variable count
+        // El do-while asegura que el bloque se ejecute al menos una vez
+    do{
+        // Verificamos si el número actual es par
+        if(numbers[i] % 2 ==0){
+            count++;
+        }
+        // Avanzamos al siguiente índice
+        i++;
+
+    }while(i<numbers.length);
+        // Sustituye el 0 por la variable count
+        return count;
     }
 
     /**
@@ -41,8 +70,17 @@ public class Loops {
         int[] numbers = {4, 6, 9, 5, 3, 2};
         // Tu código aquí (usa for)
         // consejo: declara la variable "max" fuera del loop
+        int max = numbers[0];
+        // Recorremos el array desde el segundo elemento
+        for(int i=0; i<numbers.length; i++){
+            // Si encontramos un número más grande que el actual 'mayor'
+        if(numbers[i] > max){
+            max=numbers[i];
 
-        return 0; // Sustituye el 0 por la variable max
+        }
+        }
+        // Sustituye el 0 por la variable max
+        return max;
     }
 
     public static void main(String[] args) {
